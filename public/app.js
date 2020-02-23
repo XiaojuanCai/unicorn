cfd0616b23884a672d49b80a9e2030bdbe74a793
XiaojuanCai <caixiaojuancxj@gmail.com>
2020-02-22 22:55:58 -0800

    add code for refreshing homepage every 10s, in order to pull changes bu other ranchers.

--- public/app.js
+++ public/app.js
@@ -12,10 +12,11 @@ function sendLocation(event) {
     method: 'PUT',
     data: { 'location': location, 'id' : id }
   };
+  
 
-  $.ajax(`/updateLocation`, ajaxSettings)
-    .then(() => {
-      $(`#updated_${id}`).html("Location changed to '" + location + "'.");  
+  $.ajax(`/location`, ajaxSettings)
+    .then((data) => {
+      $(`#updated_${id}`).html("Location changed to '" + data.location + "'.");  
     });
 }
 
